ec45a47438e85991d6637ec27aa931cf6ee72c4c
estasney <estasney@users.noreply.github.com>
2019-07-31 00:12:24 -0400

    fix regex

--- company_graph/normalizer.py
+++ company_graph/normalizer.py
@@ -16,7 +16,7 @@ RE_PUNCTUATION = re.compile(r'([%s])+' % re.escape(string.punctuation))
 RE_MULTIPLE_WHITESPACE = re.compile(r"( {2,})")
 STOPWORDS = ['for', 'global', 'by', 'international', 'technologies', 'the', 'group', 'systems', 'of', 'corporation',
              'and', 'limited', 'llc', 'ltd', 'inc', 'company', 'consulting']
-RE_STOPWORDS = re.compile("({})(?![A-z] )".format("|".join(STOPWORDS)), re.IGNORECASE)
+RE_STOPWORDS = re.compile("({})(?![A-z] ?)".format("|".join(STOPWORDS)), re.IGNORECASE)
 
 STRIP_PUNCTUATION = partial(strip_match, pattern=RE_PUNCTUATION)
 STRIP_MULTIPLE_WHITESPACE = partial(strip_match, pattern=RE_MULTIPLE_WHITESPACE)

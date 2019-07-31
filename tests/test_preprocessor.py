a0c0db218a2dc643e323f91fa728c14bed64a51b
estasney <estasney@users.noreply.github.com>
2019-07-30 23:46:01 -0400

    add tests

---
+++ tests/test_preprocessor.py
@@ -0,0 +1,48 @@
+import pickle
+
+from hypothesis import given, example
+from hypothesis.strategies import text, characters
+import pkg_resources
+import csv
+
+from company_graph.normalizer import *
+
+import unittest
+
+
+class TestRegex(unittest.TestCase):
+
+    def setUp(self) -> None:
+        with open('company_names_stopwords.csv', 'r', newline='', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            headers = next(reader)
+            self.test_pairs_stopwords = [tuple(line) for line in reader]
+
+    @given(text() | characters())
+    def test_strip_punc(self, s):
+        stripped_s = STRIP_PUNCTUATION(s)
+        assert len([p for p in string.punctuation if p in stripped_s]) == 0
+
+    @given(text() | characters())
+    def test_multi_ws(self, s):
+        # Are these still present?
+        copy_s = s
+
+        assert STRIP_MULTIPLE_WHITESPACE(copy_s).find("  ") == -1
+
+        # Did we remove desired whitespace?
+        original_tokens = [token for token in s.split(" ") if token != " "]
+        stripped_tokens = STRIP_MULTIPLE_WHITESPACE(copy_s).split(" ")
+        assert original_tokens == stripped_tokens
+
+    def test_stopwords(self):
+        for raw, expected in self.test_pairs_stopwords:
+            if STRIP_STOPWORDS(raw) != expected:
+                raise AssertionError("{} should have been {} but was {}".format(raw, expected, STRIP_STOPWORDS(raw)))
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()

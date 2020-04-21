c51bb0e21289ed4fe06a3aa4a4910a76571de516
estasney <estasney@users.noreply.github.com>
2020-04-21 10:33:26 -0400

    update data

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/estasney/CompanyGraph'
 EMAIL = 'estasney@users.noreply.github.com'
 AUTHOR = 'Eric Stasney'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.3.1'
+VERSION = '0.3.2'
 
 REQUIRED = [
     'networkx>=2.3'

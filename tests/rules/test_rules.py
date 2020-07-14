9f4ae5218eb5c9c741f8fa0b63e1babb6d6a56db
estasney <estasney@users.noreply.github.com>
2020-07-14 15:26:55 -0400

    add tests for checking that rules are handled correctly

--- tests/rules/test_rules.py
+++ tests/rules/test_rules.py
@@ -5,3 +5,21 @@ def cg():
     from company_graph.cg import CompanyGraph
     cg = CompanyGraph()
     return cg
+
+@pytest.mark.parametrize("given,expected,equals", [("PrefixIBM", "ibm", False),
+                                                   ("Prefix IBM", "ibm", True),
+                                                   ("Prefix Oracle", "oracle", True),
+                                                   ("Oracle Systems, Inc", "oracle", True),
+                                                   ("Prefix Accenture", "accenture", False),
+                                                   ("Accenture Suffix", "accenture", True),
+                                                   ("Hewlett Packard", "hewlett packard", True),
+                                                   ("HPE", "hewlett packard", True),
+                                                   ("AT&T", "att", True),
+                                                   ("ATT", "att", True)])
+def test_rules(given, expected, equals, cg):
+    company_id = cg(given)
+    company_str = cg(company_id)
+    if equals:
+        assert company_str == expected
+    else:
+        assert company_str != expected
